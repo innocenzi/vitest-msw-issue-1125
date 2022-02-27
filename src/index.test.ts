dcfa175fd3a5b413facf2798caca25536a5d9a02
Enzo Innocenzi <enzo@innocenzi.dev>
2022-02-27 23:50:56 +0100

    feat: add fetch test to compare

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,28 +1,34 @@
 import { it, beforeAll, afterAll, afterEach, expect } from 'vitest'
 import { server } from './server'
 import { rest } from 'msw'
-import { callAxios } from './index'
+import { callAxios, callFetch } from './index'
 
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
 afterAll(() => server.close())
 afterEach(() => server.resetHandlers())
 
-
-it('calls axios', async () => {
-	server.use(rest.get('http://localhost/visit', (req, res, ctx) => res(
+it('calls fetch', async () => {
+	server.use(rest.get('http://localhost/fetch', (req, res, ctx) => res(
 		ctx.status(200),
 		ctx.set('x-sleightful', 'true'),
 		ctx.json({
-			view: {
-				name: 'target.view',
-				properties: {
-					foo: 'bar',
-				},
-			},
+			data: 'ok'
 		}),
 	)))
 
-	const response = await callAxios('http://localhost/visit')
+	const response = await callFetch('http://localhost/fetch')
+	expect(response.data).toBe('ok')
+})
 
-	expect(response).toMatchInlineSnapshot({})
+it('calls axios', async () => {
+	server.use(rest.get('http://localhost/axios', (req, res, ctx) => res(
+		ctx.status(200),
+		ctx.set('x-sleightful', 'true'),
+		ctx.json({
+			data: 'ok'
+		}),
+	)))
+
+	const response = await callAxios('http://localhost/axios')
+	expect(response.data).not.toBeUndefined()
 })
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+export async function callFetch(url: string) {
+	return await (await fetch(url)).json()
+}
+
 export async function callAxios(url: string) {
-	return await axios.request({
-		url
-	})
+	return await axios.request({ url, method: 'get' })
 }
